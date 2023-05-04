74d4b75cc7573860bbb2cdd7e0a73aab68a20156
TomerBenShimol <tomerbenshimol3164@gmail.com>
2023-05-04 23:15:35 +0300

    Post service + post web design implementation

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Post } from '../post.model';
+import { PostsService } from '../post.service';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
+})
+export class PostCreateComponent {
+  enteredTitle = '';
+  enteredContent = '';
+
+  constructor(public postsService: PostsService) {}
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) return;
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content,
+    };
+    this.postsService.setPosts(form.value.title, form.value.content);
+    form.resetForm();
+  }
+}
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Post } from '../post.model';
+import { PostsService } from '../post.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postSub: Subscription | undefined;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.posts = this.postsService.getPosts();
+    this.postSub = this.postsService
+      .getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postSub?.unsubscribe();
+  }
+}
